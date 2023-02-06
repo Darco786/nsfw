f03f3330e520b7c820bc3ccf3eeef87fb9eea3e4
draco <122390064+Darco786@users.noreply.github.com>
2023-02-07 04:17:40 +0530

    new

--- src/components/nftUserHome/NftUserProfile.js
+++ src/components/nftUserHome/NftUserProfile.js
@@ -1,6 +1,8 @@
 import React from "react";
 import User_Img from'../../assets/user.png'
 import Verify_img from '../../assets/verify.svg'
+import {BsGlobe,BsTwitter,BsFillShareFill,BsThreeDots} from 'react-icons/bs'
+import {AiFillInstagram,AiOutlineStar} from 'react-icons/ai'
 
 function NftUserProfile() {
   return (
@@ -15,11 +17,11 @@ function NftUserProfile() {
               <div >
                 <img src={User_Img} alt="" className="profile-img" />
               </div>
-              <div className="d-flex">
+              <div className="d-flex user_name">
                 <h2>Mahsa_Collection </h2> 
-                <img src={Verify_img} alt=""/>
+                <img src={Verify_img} alt="" className="verify-img"/>
               </div>
-              <p>
+              <p className="user_details">
                 Lorem ipsum dolor sit amet consectetur. Risus ut id velit
                 hendrerit nunc tristique non malesuada malesuada. Eu lacus sed
                 imperdiet elementum ipsum duis at ridiculus rhoncus.{" "}
@@ -27,7 +29,24 @@ function NftUserProfile() {
               </div>
             </div>
 
-            <div className="col-md-6"></div>
+            <div className="col-md-6">
+                <div className="share-box">
+                    <div className="social-share">
+                        <a href="/"><BsGlobe/></a>
+                        <a href="/"><AiFillInstagram/> </a>
+                        <a href="/"><BsTwitter/> </a>
+                        <a href="/"><AiFillInstagram/> </a>
+                        <a href="/"><AiOutlineStar/> </a>
+                        <a href="/"><BsFillShareFill/> </a>
+
+                        <a href="/"><BsThreeDots/> </a>
+                        
+
+                        
+                    </div>
+                    
+                </div>
+            </div>
           </div>
         </div>
       </section>
