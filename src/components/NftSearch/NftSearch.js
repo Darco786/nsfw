8c58e5681f8aaee4012c0679ff208adb0438f70b
draco <122390064+Darco786@users.noreply.github.com>
2023-02-07 13:18:26 +0530

    new

---
+++ src/components/NftSearch/NftSearch.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {HiOutlineBars3BottomLeft} from 'react-icons/hi'
+
+function NftSearch() {
+  return (
+    <section>
+    <div className='container'>
+        <div className='search-bar-box'>
+            <div>
+
+            </div>
+            <div>
+            <div className="form-group has-search">
+    <span className="fa fa-search form-control-feedback"></span>
+    <input type="text" className="form-control" placeholder="Search"/>
+  </div>
+            </div>
+
+
+        </div>
+
+    </div>
+    </section>
+  )
+}
+
+export default NftSearch
