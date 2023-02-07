6f4b5f470c06ebff94cd0f7dc9618477a2f519b0
draco <122390064+Darco786@users.noreply.github.com>
2023-02-07 15:51:23 +0530

    new

--- src/components/NftBoxpage/NftBoxPage.js
+++ src/components/NftBoxpage/NftBoxPage.js
@@ -1,44 +1,222 @@
-import React from 'react'
-import NftSearch from '../NftSearch/NftSearch'
-import {BsThreeDots} from 'react-icons/bs'
-import {FaEthereum} from 'react-icons/fa'
-import Nft1 from '../../assets/nfts/nft2.png'
+import React from "react";
+import NftSearch from "../NftSearch/NftSearch";
+import { BsThreeDots } from "react-icons/bs";
+import { FaEthereum } from "react-icons/fa";
+import Nft1 from "../../assets/nfts/nft2.png";
+import "./NftCard.css";
 
 function NftBoxPage() {
   return (
     <>
-    <NftSearch/>
-    <section className='nft-con-sec'>
-        <div className='container'>
-            <div className='nft-grid'>
+      <NftSearch />
+      <section className="nft-con-sec">
+        <div className="container">
+          <div className="nft-grid">
+            {/* card-1 start */}
+            <div className="nft-card-box">
+              <img src={Nft1} alt=""  className="card-img"/>
+              <div className="title">
+                <h2>Purples</h2>
+                <span>
+                  <BsThreeDots />
+                </span>
+              </div>
+              <span className="nft-username">@loremipsum</span>
+              <div className="price">
+                <p>4 ETH</p>
 
-                <div className='nft-card-box'>
-                    <img src={Nft1} alt=""/>
-                    <div className='title'>
-                        <h2>Purples</h2>
-                        <span><BsThreeDots/></span>
-
-                    </div>
-                    <span>@loremipsum</span>
-                    <div className='price'>
-                        <p>4 ETH</p>
-
-                        <span><FaEthereum/></span>
-                    </div>
-                    <div>
-                        <a href="/">Buy Now</a>
-                    </div>
-                </div>
-
-
+                <span>
+                  <FaEthereum />
+                </span>
+              </div>
+              <div className="buy-nft-box">
+                <a href="/" className="btn-nft-buy">Buy Now</a>
+              </div>
             </div>
 
+ {/* card-1 end */}
+
+         {/* card-1 start */}
+         <div className="nft-card-box">
+              <img src={Nft1} alt=""  className="card-img"/>
+              <div className="title">
+                <h2>Purples</h2>
+                <span>
+                  <BsThreeDots />
+                </span>
+              </div>
+              <span className="nft-username">@loremipsum</span>
+              <div className="price">
+                <p>4 ETH</p>
+
+                <span>
+                  <FaEthereum />
+                </span>
+              </div>
+              <div className="buy-nft-box">
+                <a href="/" className="btn-nft-buy">Buy Now</a>
+              </div>
+            </div>
+
+ {/* card-1 end */}
+
+
+
+         {/* card-1 start */}
+         <div className="nft-card-box">
+              <img src={Nft1} alt=""  className="card-img"/>
+              <div className="title">
+                <h2>Purples</h2>
+                <span>
+                  <BsThreeDots />
+                </span>
+              </div>
+              <span className="nft-username">@loremipsum</span>
+              <div className="price">
+                <p>4 ETH</p>
+
+                <span>
+                  <FaEthereum />
+                </span>
+              </div>
+              <div className="buy-nft-box">
+                <a href="/" className="btn-nft-buy">Buy Now</a>
+              </div>
+            </div>
+
+ {/* card-1 end */}
+
+
+
+         {/* card-1 start */}
+         <div className="nft-card-box">
+              <img src={Nft1} alt=""  className="card-img"/>
+              <div className="title">
+                <h2>Purples</h2>
+                <span>
+                  <BsThreeDots />
+                </span>
+              </div>
+              <span className="nft-username">@loremipsum</span>
+              <div className="price">
+                <p>4 ETH</p>
+
+                <span>
+                  <FaEthereum />
+                </span>
+              </div>
+              <div className="buy-nft-box">
+                <a href="/" className="btn-nft-buy">Buy Now</a>
+              </div>
+            </div>
+
+ {/* card-1 end */}
+
+
+
+         {/* card-1 start */}
+         <div className="nft-card-box">
+              <img src={Nft1} alt=""  className="card-img"/>
+              <div className="title">
+                <h2>Purples</h2>
+                <span>
+                  <BsThreeDots />
+                </span>
+              </div>
+              <span className="nft-username">@loremipsum</span>
+              <div className="price">
+                <p>4 ETH</p>
+
+                <span>
+                  <FaEthereum />
+                </span>
+              </div>
+              <div className="buy-nft-box">
+                <a href="/" className="btn-nft-buy">Buy Now</a>
+              </div>
+            </div>
+
+ {/* card-1 end */}
+
+
+         {/* card-1 start */}
+         <div className="nft-card-box">
+              <img src={Nft1} alt=""  className="card-img"/>
+              <div className="title">
+                <h2>Purples</h2>
+                <span>
+                  <BsThreeDots />
+                </span>
+              </div>
+              <span className="nft-username">@loremipsum</span>
+              <div className="price">
+                <p>4 ETH</p>
+
+                <span>
+                  <FaEthereum />
+                </span>
+              </div>
+              <div className="buy-nft-box">
+                <a href="/" className="btn-nft-buy">Buy Now</a>
+              </div>
+            </div>
+
+ {/* card-1 end */}
+
+
+         {/* card-1 start */}
+         <div className="nft-card-box">
+              <img src={Nft1} alt=""  className="card-img"/>
+              <div className="title">
+                <h2>Purples</h2>
+                <span>
+                  <BsThreeDots />
+                </span>
+              </div>
+              <span className="nft-username">@loremipsum</span>
+              <div className="price">
+                <p>4 ETH</p>
+
+                <span>
+                  <FaEthereum />
+                </span>
+              </div>
+              <div className="buy-nft-box">
+                <a href="/" className="btn-nft-buy">Buy Now</a>
+              </div>
+            </div>
+
+ {/* card-1 end */}
+
+
+         {/* card-1 start */}
+         <div className="nft-card-box">
+              <img src={Nft1} alt=""  className="card-img"/>
+              <div className="title">
+                <h2>Purples</h2>
+                <span>
+                  <BsThreeDots />
+                </span>
+              </div>
+              <span className="nft-username">@loremipsum</span>
+              <div className="price">
+                <p>4 ETH</p>
+
+                <span>
+                  <FaEthereum />
+                </span>
+              </div>
+              <div className="buy-nft-box">
+                <a href="/" className="btn-nft-buy">Buy Now</a>
+              </div>
+            </div>
+
+ {/* card-1 end */}
+          </div>
         </div>
-        
-    </section>
+      </section>
     </>
-  
-  )
+  );
 }
 
-export default NftBoxPage
+export default NftBoxPage;
