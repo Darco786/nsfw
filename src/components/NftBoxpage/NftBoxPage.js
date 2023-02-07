8ab8dd494dbc885aef7b2ee9e468cd3a383dcb0e
draco <122390064+Darco786@users.noreply.github.com>
2023-02-07 15:41:23 +0530

    new

--- src/components/NftBoxpage/NftBoxPage.js
+++ src/components/NftBoxpage/NftBoxPage.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import NftSearch from '../NftSearch/NftSearch'
+import {BsThreeDots} from 'react-icons/bs'
+import {FaEthereum} from 'react-icons/fa'
+import Nft1 from '../../assets/nfts/nft2.png'
 
 function NftBoxPage() {
   return (
     <>
     <NftSearch/>
-    <section>
+    <section className='nft-con-sec'>
         <div className='container'>
             <div className='nft-grid'>
-                
+
+                <div className='nft-card-box'>
+                    <img src={Nft1} alt=""/>
+                    <div className='title'>
+                        <h2>Purples</h2>
+                        <span><BsThreeDots/></span>
+
+                    </div>
+                    <span>@loremipsum</span>
+                    <div className='price'>
+                        <p>4 ETH</p>
+
+                        <span><FaEthereum/></span>
+                    </div>
+                    <div>
+                        <a href="/">Buy Now</a>
+                    </div>
+                </div>
+
 
             </div>
 
